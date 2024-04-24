89a503a375ddd604f37edd8d6ec6742361b4ea32
Igor Grieder Melo <igorgrieder@gmail.com>
2024-04-24 09:55:26 -0300

    Inicio do Projeto

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/prefer-interface": "off",
+        "@typescript-eslint/consistent-type-definitions": "off"
+    },
+    root: true,
+    extends: ["plugin:tailwindcss/recommended"],
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx', '*.js'],
+            parser: '@typescript-eslint/parser',
+        },
+    ],
+}
