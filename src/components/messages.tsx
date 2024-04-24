d0e50c4bb85d9374b7be82dcd7b62878e90ea100
Igor Grieder Melo <igorgrieder@gmail.com>
2024-04-24 18:59:34 -0300

    ChatBox finalizada, implementando horario as mensagens

--- src/components/messages.tsx
+++ src/components/messages.tsx
@@ -1,14 +1,15 @@
 type Props = {
   whoSent: string
+  body: string
 }
 
-const Message = ({ whoSent }: Props) => {
+const Message = ({ whoSent, body }: Props) => {
   return (
     <div className={`flex w-full bg-transparent ${whoSent === 'bot' ? (`justify-start`) : (`justify-end`)} mb-5`}>
       {/* whoSent vai determinar se o alinhamento dos items estara a direita ou a esquerda*/}
-      <div className="flex max-w-[300px] flex-col items-start rounded-xl border border-gray-500 px-8 py-3">
-        <h1 className="mb-2 font-bold">Quem Mandou</h1>
-        <p className="max-w-[20ch] text-base font-light">Mensagem triste de quem mandou uau</p>
+      <div className="flex max-w-[300px] flex-col items-start rounded-xl border border-gray-500 py-3 pl-3 pr-8">
+        <h1 className="mb-2 font-bold capitalize">{whoSent}</h1>
+        <p className="max-w-[20ch] text-base font-light">{body}</p>
       </div>
     </div>
   )
