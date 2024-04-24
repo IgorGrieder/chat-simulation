010e1935a847bb6cf6d0b5bbbf09ddba2f448c65
Igor Grieder Melo <igorgrieder@gmail.com>
2024-04-24 18:52:37 -0300

    Adicionando mensagens corretamente no BoxShow

--- src/components/chatInputBot.tsx
+++ src/components/chatInputBot.tsx
@@ -1,10 +1,32 @@
+import { chatHistoryCtx } from "@/context/contextChat"
+import { useContext, useState } from "react"
+
 const ChatInputBot = () => {
+  const chatCtx = useContext(chatHistoryCtx)
+  const [input, setInput] = useState('')
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault() // previnir de atualizar a pagina
+    if (input !== '') { // checando se o input esta vazio para enviar uma mensagem
+      chatCtx?.dispatch({ // adicionando no reducer
+        type: 'add',
+        payload: {
+          sentBy: 'bot',
+          body: input
+        }
+      })
+    setInput('') // limpando area de input
+    }
+  }
+  
   return (
-    <form action="" className="mb-3 flex w-full border-b border-white pb-3">
+    <form className="mb-3 flex w-full border-b border-white pb-3" onSubmit={handleSubmit}>
       <input
         type="text"
         className="w-full bg-transparent text-gray-900 outline-none"
         placeholder="Bot, digite sua mensagem e tecle enter"
+        value={input}
+        onChange={(event) => { setInput(event.target.value) }}
       />
     </form>
   )
--- src/components/chatinputPerson.tsx
+++ src/components/chatinputPerson.tsx
@@ -1,14 +1,38 @@
+import { chatHistoryCtx } from "@/context/contextChat";
+import { useContext, useState } from "react";
+
 type Props = {
   userName: string;
 }
 
 const ChatInputPerson = ({ userName }: Props) => {
+  const chatCtx1 = useContext(chatHistoryCtx)
+  const [input, setInput] = useState("")
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault() // previnir de atualizar a pagina
+    if (input !== "") {
+      // checando se o input esta vazio para enviar uma mensagem
+      chatCtx1?.dispatch({
+        // adicionando no reducer
+        type: "add",
+        payload: {
+          sentBy: userName,
+          body: input,
+        },
+      })
+      setInput("") // limpando area de input
+    }
+  }
+
   return (
-    <form action="" className="flex w-full">
+    <form className="flex w-full" onSubmit={handleSubmit}>
       <input
         type="text"
         className="w-full bg-transparent text-gray-900 outline-none"
         placeholder={`${userName}, digite sua mensagem e tecle enter`}
+        value={input}
+        onChange={(event) => { setInput(event.target.value) }}
       />
     </form>
   )
